120759bcd967c3f9c130bd8ef24964d5f0d626c6
bitboi-ptit <kpcuongz@gmail.com>
2025-09-13 00:48:47 +0700

    re-design layouts. install and connect database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const database = require("./config/database");
 const route = require("./routes/client/index.route");
+
 require('dotenv').config(); 
-
 const port = process.env.PORT;
-
+database.connect();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
