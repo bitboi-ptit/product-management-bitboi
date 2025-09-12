f104c0913afd3bbf894657e919739a90256df158
bitboi-ptit <kpcuongz@gmail.com>
2025-09-12 12:34:00 +0700

    configure folder controller

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,8 +1,6 @@
 const express = require("express"); // import để sử dụng hàm Router trong express
 const router = express.Router();
+const controller = require("../../controllers/client/product.controller");
 
-router.get("/",(req,res)=>{
-    res.render("client/pages/products/index");
-});
-
+router.get("/",controller.index);
 module.exports = router;
