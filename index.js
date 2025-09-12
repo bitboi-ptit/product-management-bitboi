ec35ba93f3b5630f1edcdc267a1ac7b33151ae88
bitboi-ptit <kpcuongz@gmail.com>
2025-09-12 12:56:24 +0700

    configure .env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const route = require("./routes/client/index.route");
+require('dotenv').config(); 
+
+const port = process.env.PORT;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
