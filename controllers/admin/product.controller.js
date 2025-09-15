806f0af88d3414673a26ca4b9205beb87c8249ce
bitboi-ptit <kpcuongz@gmail.com>
2025-09-15 14:44:20 +0700

    Load product data and display it

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,11 @@
 //[Get] /admin/products
-module.exports.index=(req,res)=>{ 
-    res.render("admin/pages/product/index");
+const Product = require("../../models/product.model");
+module.exports.index=async (req,res)=>{ 
+    const products = await Product.find({
+        deleted : false
+    })
+    res.render("admin/pages/product/index",{
+        pageTitle : "Trang danh sách sản phẩm",
+        products: products
+    });
 }
