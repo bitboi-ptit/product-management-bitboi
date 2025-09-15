10ca310b8de0ac1d08338ba47265c5fc88f75091
bitboi-ptit <kpcuongz@gmail.com>
2025-09-15 14:09:56 +0700

    reate a product list interface

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,8 +1,6 @@
-const systemConfig = require("../../config/system");
 
 module.exports.dashboard = (req, res) => {
     res.render("admin/pages/dashboard/index", {
         pageTitle: "Trang tổng quan",
-        PATH_ADMIN: systemConfig.prefixAdmin
     });
 };
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,9 @@
+const { prefixAdmin } = require("../../config/system");
 const dashboardRoute = require("./dashboard.route");
-const systemConfig = require("../../config/system");
+const productRoute = require("./product.route");
+//const systemConfig = require("../../config/system");
 module.exports =(app)=>{
-    const PATH_ADMIN = systemConfig.prefixAdmin;
-    app.use(PATH_ADMIN+"/dashboard",dashboardRoute);
+    //const PATH_ADMIN = systemConfig.prefixAdmin;
+    app.use(`${prefixAdmin}/dashboard`,dashboardRoute);
+    app.use(`${prefixAdmin}/products`,productRoute);
 }
