fbf3ccbf7c4bf95566f487dad3ebf03c380b1155
bitboi-ptit <kpcuongz@gmail.com>
2025-09-18 09:24:27 +0700

    Finding function

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -31,12 +31,19 @@ module.exports.index = async (req, res) => {
     if (req.query.status) {
         find.status = req.query.status
     }
+    let keyword="";
+    if(req.query.keyword){
+        keyword=req.query.keyword;
 
+        const regex = new RegExp(keyword,"i");
+        find.title=regex;
+    }
 
     const products = await Product.find(find);
     res.render("admin/pages/product/index", {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -6,16 +6,14 @@ if (buttonsStatus.length > 0) {
     let url = new URL(window.location.href);
     buttonsStatus.forEach(button => {
         button.addEventListener("click", () => {
-        
+
             const status = button.getAttribute("button-status");
-           
+
             if (status) {
                 url.searchParams.set("status", status);
             } else {
                 url.searchParams.delete("status");
             }
-
-            console.log(url.href);
             window.location.href = url.href;
         });
 
@@ -25,3 +23,19 @@ if (buttonsStatus.length > 0) {
 }
 
 //end button-status
+//form-search
+const formSearch = document.querySelector("#form-search");
+if (formSearch.length) {
+    let url = new URL(window.location.href);
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if (keyword) {
+            url.searchParams.set("keyword", keyword);
+        } else {
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    });
+}
+//end form-search
