3971c7cc9f2e582b0c020120db19795d21bf1787
bitboi-ptit <kpcuongz@gmail.com>
2025-09-18 10:46:59 +0700

    optimize search section

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 //[Get] /admin/products
 const Product = require("../../models/product.model");
 const filterStatusHelper= require("../../helpers/filterStatus");
+const searchHelper= require("../../helpers/search");
 module.exports.index = async (req, res) => {
     const filterStatus = filterStatusHelper(req.query);
     
@@ -10,12 +11,10 @@ module.exports.index = async (req, res) => {
     if (req.query.status) {
         find.status = req.query.status
     }
-    let keyword="";
-    if(req.query.keyword){
-        keyword=req.query.keyword;
 
-        const regex = new RegExp(keyword,"i");
-        find.title=regex;
+    const objectSearch = searchHelper(req.query);
+    if(objectSearch.regex){
+        find.title=objectSearch.regex;
     }
 
     const products = await Product.find(find);
@@ -23,6 +22,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword
     });
 }
