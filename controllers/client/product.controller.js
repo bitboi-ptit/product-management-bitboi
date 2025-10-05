4a672343c520258a81cbb873db9c4e6062c0d919
bitboi-ptit <kpcuongz@gmail.com>
2025-10-05 21:48:19 +0700

    detail page on client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -13,9 +13,27 @@ module.exports.index = async (req, res) => {
         item.newPrice = (item.price - item.price * (item.discountPercentage / 100)).toFixed(0);
         return item;
     });
-    console.log(newProducts);
     res.render("client/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
         products: newProducts // trả products ra file index.pug (product)
     });
+}
+//[Get] /products/:slug
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            status:'active',
+            slug: req.params.slug
+        };
+        const product = await Product.findOne(find);
+        res.render("client/pages/products/detail", {
+            pageTitle: product.title,
+            product: product
+        });
+
+    } catch (error) {
+        res.redirect(`/products`);
+    }
+
 }
