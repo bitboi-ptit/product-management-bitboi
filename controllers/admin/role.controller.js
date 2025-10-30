4c53da57cec2b489b29c27241e66b291f94a189e
bitboi-ptit <kpcuongz@gmail.com>
2025-10-30 15:38:49 +0700

    Group permissions + edit

---
+++ controllers/admin/role.controller.js
@@ -0,0 +1,55 @@
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
+
+//[GET]admin/roles
+module.exports.index = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+    const records = await Role.find(find);
+    res.render("admin/pages/roles/index", {
+        pageTitle: "Trang Nhóm Quyền",
+        records: records
+    });
+};
+//[GET]admin/roles/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create", {
+        pageTitle: "Tạo Nhóm Quyền"
+    });
+};
+//[POST]admin/roles/create
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
+//[GET]admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            _id: req.params.id
+        };
+        const data = await Role.findOne(find);
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa Nhóm Quyền",
+            data: data
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+};
+//[PATCH]admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id =req.params.id;
+        await Role.updateOne({_id:id},req.body);
+        
+        req.flash('success', 'Cập nhật thành công!');
+    } catch (error) {
+        req.flash('error', 'Cập nhật không thành công!');
+    }
+    const backURL= req.get('Referer');
+    res.redirect(`${backURL}`);
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,11 +1,15 @@
-const { prefixAdmin } = require("../../config/system");
+const {
+    prefixAdmin
+} = require("../../config/system");
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
+const roleRoutes = require("./role.route");
 //const systemConfig = require("../../config/system");
-module.exports =(app)=>{
+module.exports = (app) => {
     //const PATH_ADMIN = systemConfig.prefixAdmin;
-    app.use(`${prefixAdmin}/dashboard`,dashboardRoutes);
-    app.use(`${prefixAdmin}/products`,productRoutes);
-    app.use(`${prefixAdmin}/products-category`,productCategoryRoutes);
+    app.use(`${prefixAdmin}/dashboard`, dashboardRoutes);
+    app.use(`${prefixAdmin}/products`, productRoutes);
+    app.use(`${prefixAdmin}/products-category`, productCategoryRoutes);
+    app.use(`${prefixAdmin}/roles`, roleRoutes);
 }
